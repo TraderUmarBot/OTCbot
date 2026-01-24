76ef1d6250a10cc382fea6783deca5f8904e0776
UmarBot <2t78zmwktx@privaterelay.appleid.com>
2026-01-24 12:40:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ logger = logging.getLogger(__name__)
 # ⚙️ КОНФИГУРАЦИЯ БОТА
 # ============================================
 
-TOKEN = "8578509228:AAFrXSuv5WV8oWWvZkVZL_i9E4kB7LDQBu0"
+TOKEN = "8578509228:AAFdsHJOSaNc0b1JrCnRwAbA-d4IVXI0Ip0"
 ADMIN_IDS = {6117198446, 7079260196}
 ADMIN_USER = "@Kuruttrader"
 ADMIN_LINK = "https://t.me/Kuruttrader"
